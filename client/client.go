0d2919954681736b4920b07ad8582bc35d474ef4
jodydadescott <jody@thescottsweb.com>
2022-10-13 15:39:03 -0500

    init2

--- client/client.go
+++ client/client.go
@@ -4,46 +4,89 @@ import (
 	"context"
 	"io"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	pb "github.com/jodydadescott/go-protobuf-example/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
-	// dial server
-	// conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+
+	err := run()
+
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+}
+
+func run() error {
 
 	conn, err := grpc.Dial("passthrough:///unix:///tmp/proto.sock", grpc.WithInsecure())
 
 	if err != nil {
-		log.Fatalf("can not connect with server %v", err)
+		return err
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	// create stream
 	client := pb.NewStreamServiceClient(conn)
 	in := &pb.Request{Id: 1}
-	stream, err := client.FetchResponse(context.Background(), in)
+	stream, err := client.FetchResponse(ctx, in)
 	if err != nil {
-		log.Fatalf("open stream error %v", err)
+		cancel()
+		return err
 	}
 
-	done := make(chan bool)
+	streamdone := make(chan error)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("Waiting on signal")
+		<-sig
+		log.Printf("Got signal")
+		cancel()
+	}()
 
 	go func() {
 		for {
 			resp, err := stream.Recv()
 			if err == io.EOF {
-				done <- true //means stream is finished
+				streamdone <- nil
 				return
 			}
+
 			if err != nil {
-				log.Fatalf("cannot receive %v", err)
+				streamdone <- err
+				return
 			}
+
+			if err != nil {
+				if e, ok := status.FromError(err); ok {
+					switch e.Code() {
+					case codes.Canceled:
+						log.Printf("It was cancelled")
+						streamdone <- err
+						return
+					}
+				}
+				streamdone <- err
+				return
+			}
+
 			log.Printf("Resp received: %s", resp.Result)
 		}
 	}()
 
-	<-done //we will wait until all response is received
-	log.Printf("finished")
+	log.Printf("waiting on stream to end")
+	<-streamdone //we will wait until all response is received
+	log.Printf("stream ended")
+	return err
 }
--- server/server.go
+++ server/server.go
@@ -1,62 +1,200 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	pb "github.com/jodydadescott/go-protobuf-example/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type server struct{}
-
-func (s *server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
-
-	log.Printf("fetch response for id : %d", in.Id)
-
-	//use wait group to allow process to be concurrent
-	var wg sync.WaitGroup
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		go func(count int64) {
-			defer wg.Done()
-
-			//time sleep to simulate server process time
-			time.Sleep(time.Duration(count) * time.Second)
-			resp := pb.Response{Result: fmt.Sprintf("Request #%d For Id:%d", count, in.Id)}
-			if err := srv.Send(&resp); err != nil {
-				log.Printf("send error %v", err)
-			}
-			log.Printf("finishing request number : %d", count)
-		}(int64(i))
-	}
-
-	wg.Wait()
-	return nil
+type server struct {
+	sync.Mutex
+	xmap       map[string]*handler
+	grpcServer *grpc.Server
 }
 
-func main() {
-	// create listiner
-	// lis, err := net.Listen("tcp", ":9000")
+func newServer() *server {
+	return &server{
+		xmap: make(map[string]*handler),
+	}
+}
+
+func (t *server) start() error {
+
+	if t.grpcServer != nil {
+		return fmt.Errorf("Duplicate call to start()")
+	}
 
 	lis, err := net.Listen("unix", "/tmp/proto.sock")
 
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return err
 	}
 
-	// create grpc server
-	s := grpc.NewServer()
-	pb.RegisterStreamServiceServer(s, &server{})
+	t.grpcServer = grpc.NewServer()
+	pb.RegisterStreamServiceServer(t.grpcServer, t)
 
 	log.Println("start server")
-	// and start...
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	return t.grpcServer.Serve(lis)
+}
+
+func (t *server) shutdown() {
+
+	log.Printf("(server) shutdown()")
+
+	if t.grpcServer == nil {
+		return
+	}
+
+	handlers := t.handlers()
+
+	if len(handlers) > 0 {
+		for _, v := range handlers {
+			v.shutdown()
+		}
+	} else {
+		log.Printf("No current handlers")
+	}
+
+	t.grpcServer.GracefulStop()
+}
+
+type handler struct {
+	*server
+	key    string
+	closed chan struct{}
+	wg     sync.WaitGroup
+}
+
+func (t *server) newHandler() *handler {
+	t.Lock()
+	defer t.Unlock()
+	h := &handler{
+		server: t,
+		closed: make(chan struct{}),
+		key:    random(),
+	}
+	t.xmap[h.key] = h
+	log.Printf("New handler %s", h.key)
+	return h
+}
+
+func (t *handler) shutdown() {
+	log.Printf("(handler=%s) shutdown()", t.key)
+	close(t.closed)
+	t.wg.Wait()
+}
+
+func (t *handler) drop() {
+	log.Printf("(handler=%s) drop()", t.key)
+	t.Lock()
+	defer t.Unlock()
+	delete(t.xmap, t.key)
+}
+
+func (t *server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
+	h := t.newHandler()
+	return h.fetchResponse(in, srv)
+}
+
+func (t *server) handlers() []*handler {
+	t.Lock()
+	defer t.Unlock()
+	var handlers []*handler
+	for _, v := range t.xmap {
+		handlers = append(handlers, v)
+	}
+	return handlers
+}
+
+func (t *handler) fetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
+
+	t.wg.Add(1)
+	defer t.wg.Done()
+	defer t.drop()
+
+	log.Printf("fetch response for id : %d", in.Id)
+
+	count := 0
+	ticker := time.NewTicker(time.Duration(2) + time.Second)
+	defer ticker.Stop()
+
+	for {
+
+		select {
+		case <-t.closed:
+			log.Printf("<-closed")
+			ticker.Stop()
+			return nil
+
+		case <-ticker.C:
+			log.Printf("<-ticker")
+			//time sleep to simulate server process time
+			resp := pb.Response{Result: fmt.Sprintf("Request #%d For Id:%d", count, in.Id)}
+			err := srv.Send(&resp)
+
+			if err != nil {
+				if e, ok := status.FromError(err); ok {
+					switch e.Code() {
+					case codes.Unavailable:
+						log.Printf("Listener went away")
+
+						return nil
+					}
+				}
+
+				log.Printf("Something is wrong %v", err)
+				return err
+			}
+
+			log.Printf("finishing request number : %d", count)
+
+			count++
+		}
+
+	}
+
+}
+
+func random() string {
+	bytes := make([]byte, 32)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic("could not generate nonce")
+	}
+	return base64.URLEncoding.EncodeToString(bytes)
+}
+
+func main() {
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	server := newServer()
+
+	go func() {
+		log.Printf("Blocing on <-sig")
+		<-sig
+		log.Printf("NOT Blocing on <-sig")
+		server.shutdown()
+	}()
+
+	log.Printf("Starting server")
+
+	err := server.start()
+	if err != nil {
+		log.Fatalf("Something is wrong %v", err)
 	}
 
 }
